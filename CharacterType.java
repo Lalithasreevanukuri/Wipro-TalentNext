681feae77991bfa70bee41f77d79686ee643c7ff
Lalithasreevanukuri <lalithasree14322@gmail.com>
2026-07-12 12:50:26 +0530

    Added If Statement programs

---
+++ CharacterType.java
@@ -0,0 +1,15 @@
+public class CharacterType {
+
+    public static void main(String[] args) {
+
+        char ch = 'A';   // Change this value to test
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            System.out.println("Alphabet");
+        } else if (ch >= '0' && ch <= '9') {
+            System.out.println("Digit");
+        } else {
+            System.out.println("Special Character");
+        }
+    }
+}
