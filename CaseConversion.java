c4879c30359502ddebb0809d6a3ab05738bb6a64
Lalithasreevanukuri <lalithasree14322@gmail.com>
2026-07-12 13:07:20 +0530

    Added CaseConversion program

---
+++ CaseConversion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CaseConversion {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println(ch + "->" + Character.toUpperCase(ch));
+        } 
+        else if (ch >= 'A' && ch <= 'Z') {
+            System.out.println(ch + "->" + Character.toLowerCase(ch));
+        }
+        else {
+            System.out.println("Not an alphabet");
+        }
+
+        sc.close();
+    }
+}
