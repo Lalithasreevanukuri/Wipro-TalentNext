a651fc4c3d6bf69e0bdb2d215a43ece65a953d0a
Lalithasreevanukuri <lalithasree14322@gmail.com>
2026-07-12 12:32:59 +0530

    Added LastDigit program

---
+++ LastDigit.java
@@ -0,0 +1,14 @@
+public class LastDigit {
+
+    public static void main(String[] args) {
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        if ((num1 % 10) == (num2 % 10)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
