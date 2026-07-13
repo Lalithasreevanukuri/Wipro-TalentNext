94e3266ce5ac9e029b129ef33994fc879d2e60b9
Lalithasreevanukuri <lalithasree14322@gmail.com>
2026-07-13 16:18:37 +0530

    Added One Dimensional Array programs

---
+++ RemoveDuplicate.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array after removing duplicates:");
+
+        for(int i = 0; i < n; i++) {
+
+            int count = 0;
+
+            for(int j = 0; j < i; j++) {
+                if(arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+
+            if(count == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
